b44a6d86c35ea9db4ff41911e8e1ebd1138e1547
Alexander <pchp250g8@tutamail.com>
2026-01-14 16:18:16 +0300

    Добавьте файлы проекта.

---
+++ ArithmMeanValueOfArray/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("ArithmMeanValueOfArray")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ArithmMeanValueOfArray")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("d419e135-11e3-4453-8c1a-29e83ac5272c")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
